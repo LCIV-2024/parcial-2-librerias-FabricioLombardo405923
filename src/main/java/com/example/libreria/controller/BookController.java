6816b9e3b7b5d12bb73c0f32b1465337bafe48e9
FABRICIO LOMBARDO <405923@tecnicatura.frc.utn.edu.ar>
2025-11-14 21:01:21 -0300

    Anda

--- src/main/java/com/example/libreria/controller/BookController.java
+++ src/main/java/com/example/libreria/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.libreria.controller;
 import com.example.libreria.dto.BookResponseDTO;
 import com.example.libreria.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-@RequiredArgsConstructor
 public class BookController {
-    
-    private final BookService bookService;
+
+    @Autowired
+    private BookService bookService;
     
     @PostMapping("/sync")
     public ResponseEntity<String> syncBooks() {
